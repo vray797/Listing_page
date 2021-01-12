91da9e32502516d74e7ef51511857f01bc235c86
vray797 <vray797@gmail.com>
2021-01-12 22:05:26 +0530

    listing page

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/header";
+import MainContent from "./components/mainContent";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+	<div className="container">
+	  <Header />
+	  <MainContent />
+	  <Footer />
+	</div>
+  );
+}
+
+export default App;
